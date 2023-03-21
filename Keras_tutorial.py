cdd55e0ec23b2169958c8926fe4dea78d59ddb1d
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-03-21 20:54:24 -0300

    Delete Keras_tutorial.py

--- Keras_tutorial.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.layers import TextVectorization
-import numpy as np
-
-# Example training data, of dtype `string`.
-training_data = np.array([["This is the 1st sample."], ["And here's the 2nd sample."]])
